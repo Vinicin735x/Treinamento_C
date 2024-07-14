7a3b10fc69097e530af1248bbff22c94240fbf95
Vinicin735x <vini08.mantovani@gmail.com>
2024-07-14 16:28:32 -0300

    Programa simples

---
+++ Calculadora_simples/main.c
@@ -0,0 +1,13 @@
+int main()
+{
+    int num1, num2, soma;
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+
+    soma = (num1 + num2);
+    printf("A soma dos numeros é %d: ", soma);
+    return 0;
+}
