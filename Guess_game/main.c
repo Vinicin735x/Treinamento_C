bc2130b28f5877ca9fcbd2a1ab5059386e3553cc
Vinicin735x <vini08.mantovani@gmail.com>
2024-07-14 18:25:14 -0300

    Adivinhe o numero

--- Guess_game/main.c
+++ Guess_game/main.c
@@ -5,18 +5,21 @@ int main()
     int tentativas = 0;
     int limite_tentativas = 3;
     int palpites = 0;
+    int limite_palpites = 2;
 
     while(guess != numero_secreto && palpites == 0){
         if(tentativas < limite_tentativas){
             printf("Digite um número: ");
             scanf("%d", &guess);
+            printf("Restam %d palpites\n", limite_palpites);
             tentativas++;
+            limite_palpites = (limite_palpites - 1);
         } else {
             palpites = 1;
         }
     }
     if(palpites == 1){
-        printf("Acabaram os palpites");
+        printf("acabaram os palpites, você errou");
     } else {
         printf("Você acertou!");
     }
